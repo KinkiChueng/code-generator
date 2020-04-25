10dbf2215d50a83f151c7f231af798e5174c8f98
KinkiChueng <719607746@qq.com>
2020-04-25 21:01:43 +0800

    创建服务client

---
+++ generator-controller/src/main/java/com/code/generator/controller/CodeGeneratorClientApplication.java
@@ -0,0 +1,17 @@
+package com.code.generator.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by lasia on 2020/4/25.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class CodeGeneratorClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGeneratorClientApplication.class, args);
+    }
+}
